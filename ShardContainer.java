0c2fc8b07bb1c5324b823f6e4ed9befb1b8d8d93
CreateWater <blackrp001@naver.com>
2024-03-28 15:54:14 +0900

    Refactor: ShardContainer

--- ShardContainer.java
+++ ShardContainer.java
@@ -1,46 +1,66 @@
+import java.util.TreeMap;
+
 public class ShardContainer {
-    private  int max_shard, skura_shard;
-    // Should Check Special Shard
+    private int maxShard;
+    private TreeMap<Integer, Integer> shard;
 
     // Constructor
     ShardContainer(int max, int shard_num) {
-        this.max_shard = max;
-        this.skura_shard = shard_num;
+        this.maxShard = max;
+        this.shard = new TreeMap<Integer, Integer>();
+        this.shard.put(0, shard_num);
     }
 
     // Method
 
     /// info
-    public int getShardNum() {
-        return this.skura_shard;
+    public int getShardNum(int target) {
+        return this.shard.get(target);
     }
+
     public int getMaxNum() {
-        return this.max_shard;
+        return this.maxShard;
     }
+
+    public int getAllShard() {
+        int result = 0;
+        for (int value : this.shard.values()){
+            result += value;
+        }
+        return result;
+    }
+
     public boolean checkCapacity(int num) {
-        if (num <= 0)
+        if (this.maxShard < 0)
             return true;
-        else if (this.max_shard - this.skura_shard >= 0)
+        else if (this.maxShard - this.getShardNum(0) >= num)
             return true;
         else
             return false;
     }
 
-    /// Move
-    public boolean decreaseShard(int num) {
-        if (this.skura_shard < num)
-            return false;
-        else {
-            this.skura_shard =- num;
-            return true;
-        }
-    }
-    public boolean increaseShard(int num) {
-        if (checkCapacity(num)){
-            this.skura_shard =+ num;
-            return true;
+    
+    public boolean decreaseShard(int target, int num) {
+        if (this.getShardNum(target) >= num){
+            int shardNum = this.shard.get(target);
+            this.shard.put(target, shardNum-num);
+
+            if (target != 0) // Not Sakura
+                this.shard.remove(target);
         }
         else
             return false;
+            
+        return true;
+    }
+    public boolean increaseShard(int target, int num) {
+        if (this.checkCapacity(num)){
+            int shardNum = this.shard.get(target);
+            this.shard.put(target, shardNum+num);
+        }
+        else
+            return false;
+            
+        return true;
     } 
 }
